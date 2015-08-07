fa4487738532210a23bdfb52d66a8b0a7a37254d
ftyjincheng <654296661@qq.com>
2015-08-07 10:25:58 +0800

    更新静态页

--- CoursePlatForm/Controllers/IndexController.cs
+++ CoursePlatForm/Controllers/IndexController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace CoursePlatForm.Controllers
 {
     public class IndexController : Controller
@@ -15,9 +16,20 @@ namespace CoursePlatForm.Controllers
         public ActionResult ShowIndex()
         {
 
-            Log4NetHelper.WriteLog(typeof(IndexController), "测试Log4Net日志是否写入");
+            //Log4NetHelper.WriteLog(typeof(IndexController), "测试Log4Net日志是否写入");
             return View();
         }
 
+        public PartialViewResult LeftMenu_Partial()
+        {
+            string s = "return好了";
+            ViewBag.s = s;
+            return PartialView();
+        }
+
+        public ActionResult index()
+        {
+            return View();
+        }
     }
 }
